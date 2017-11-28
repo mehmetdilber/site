18f72ac16d5be38a2367d74786f5bf0a1ae1db9b
Mehmet Dilber <17550440014@student.beykent.edu.tr>
2017-11-28 19:57:09 +0000

    *

---
+++ admin/config.php
@@ -0,0 +1,30 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://139.59.138.107/admin/');
+define('HTTP_CATALOG', 'http://139.59.138.107/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://139.59.138.107/admin/');
+define('HTTPS_CATALOG', 'http://139.59.138.107/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/releases/1/admin/');
+define('DIR_SYSTEM', '/var/www/html/releases/1/system/');
+define('DIR_LANGUAGE', '/var/www/html/releases/1/admin/language/');
+define('DIR_TEMPLATE', '/var/www/html/releases/1/admin/view/template/');
+define('DIR_CONFIG', '/var/www/html/releases/1/system/config/');
+define('DIR_IMAGE', '/var/www/html/releases/1/image/');
+define('DIR_CACHE', '/var/www/html/releases/1/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/releases/1/system/download/');
+define('DIR_UPLOAD', '/var/www/html/releases/1/system/upload/');
+define('DIR_LOGS', '/var/www/html/releases/1/system/logs/');
+define('DIR_MODIFICATION', '/var/www/html/releases/1/system/modification/');
+define('DIR_CATALOG', '/var/www/html/releases/1/catalog/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '6d189eb90f7ed114aaa8ea2e2b40c8787cce7973f9351f82');
+define('DB_DATABASE', 'arcelik');
+define('DB_PREFIX', 'oc_');
---
+++ config.php
@@ -0,0 +1,27 @@
+<?php
+// HTTP
+define('HTTP_SERVER', 'http://139.59.138.107/');
+
+// HTTPS
+define('HTTPS_SERVER', 'http://139.59.138.107/');
+
+// DIR
+define('DIR_APPLICATION', '/var/www/html/releases/1/catalog/');
+define('DIR_SYSTEM', '/var/www/html/releases/1/system/');
+define('DIR_LANGUAGE', '/var/www/html/releases/1/catalog/language/');
+define('DIR_TEMPLATE', '/var/www/html/releases/1/catalog/view/theme/');
+define('DIR_CONFIG', '/var/www/html/releases/1/system/config/');
+define('DIR_IMAGE', '/var/www/html/releases/1/image/');
+define('DIR_CACHE', '/var/www/html/releases/1/system/cache/');
+define('DIR_DOWNLOAD', '/var/www/html/releases/1/system/download/');
+define('DIR_UPLOAD', '/var/www/html/releases/1/system/upload/');
+define('DIR_MODIFICATION', '/var/www/html/releases/1/system/modification/');
+define('DIR_LOGS', '/var/www/html/releases/1/system/logs/');
+
+// DB
+define('DB_DRIVER', 'mysqli');
+define('DB_HOSTNAME', 'localhost');
+define('DB_USERNAME', 'root');
+define('DB_PASSWORD', '6d189eb90f7ed114aaa8ea2e2b40c8787cce7973f9351f82');
+define('DB_DATABASE', 'arcelik');
+define('DB_PREFIX', 'oc_');
