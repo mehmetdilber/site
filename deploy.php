309b06dd0e3ed43d971b87c72aed2bc0c5f2335d
ahmetyuruk <ahmet_yuruk19@hotmail.com>
2017-11-28 22:59:47 +0300

    *

--- deploy.php
+++ deploy.php
@@ -4,26 +4,34 @@ namespace Deployer;
 require 'recipe/common.php';
 
 // Project name
-set('application', 'my_project');
+set('application', 'arcelik');
 
 // Project repository
-set('repository', '');
+set('repository', 'git@github.com:mehmetdilber/site.git');
 
 // [Optional] Allocate tty for git clone. Default value is false.
 set('git_tty', true); 
 
 // Shared files/dirs between deploys 
-set('shared_files', []);
+set('shared_files', [
+    "config.php",
+    "admin/config.php"
+]);
 set('shared_dirs', []);
 
 // Writable dirs by web server 
-set('writable_dirs', []);
+set('writable_dirs', [
+    "system/cache",
+    "system/logs"
+]);
 
 
 // Hosts
 
-host('188.166.90.155')
-    ->set('deploy_path', '/var/www/html');
+host('139.59.138.107')
+    ->set('deploy_path', '/var/www/html')
+    ->user('root')
+;
     
 
 // Tasks
@@ -37,7 +45,7 @@ task('deploy', [
     'deploy:update_code',
     'deploy:shared',
     'deploy:writable',
-    'deploy:vendors',
+   # 'deploy:vendors',
     'deploy:clear_paths',
     'deploy:symlink',
     'deploy:unlock',
